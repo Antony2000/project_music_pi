f37b1db6e06bdd48ae1390c79100a7567971a09a
Antony2000 <ajdu244@gmail.com>
2023-11-13 21:33:54 +0100

    added effects

--- effects/effect5.py
+++ effects/effect5.py
@@ -22,46 +22,46 @@ for i in range(5):
     for x in arr_pin1:
       GPIO.output(x, False)
       time.sleep(0.1)
-    for i in range (2):
-        for x in arr_pin1:
-            GPIO.output(x, False)
-        time.sleep(0.5)
+    for i in range (4):
         for x in arr_pin1:
             GPIO.output(x, True)
-        time.sleep(0.5)
+        time.sleep(0.2)
+        for x in arr_pin1:
+            GPIO.output(x, False)
+        time.sleep(0.2)
 
     for x in arr_pin2:
       GPIO.output(x, False)
       time.sleep(0.1)
-    for i in range (2):
-        for x in arr_pin2:
-            GPIO.output(x, False)
-        time.sleep(0.5)
+    for i in range (4):
         for x in arr_pin2:
             GPIO.output(x, True)
-        time.sleep(0.5)
+        time.sleep(0.2)
+        for x in arr_pin2:
+            GPIO.output(x, False)
+        time.sleep(0.2)
 
     for x in arr_pin3:
       GPIO.output(x, False)
       time.sleep(0.1)
-    for i in range (2):
-        for x in arr_pin3:
-            GPIO.output(x, False)
-        time.sleep(0.5)
+    for i in range (4):
         for x in arr_pin3:
             GPIO.output(x, True)
-        time.sleep(0.5)
+        time.sleep(0.2)
+        for x in arr_pin3:
+            GPIO.output(x, False)
+        time.sleep(0.2)
 
     for x in arr_pin4:
       GPIO.output(x, False)
       time.sleep(0.1)
-    for i in range (2):
-        for x in arr_pin4:
-            GPIO.output(x, False)
-        time.sleep(0.5)
+    for i in range (4):
         for x in arr_pin4:
             GPIO.output(x, True)
-        time.sleep(0.5)
+        time.sleep(0.2)
+        for x in arr_pin4:
+            GPIO.output(x, False)
+        time.sleep(0.2)
 
 
 for x in all_arr:
