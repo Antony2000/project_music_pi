b5effe91517b5de9459de6ba94f6810f362a2827
Antony2000 <ajdu244@gmail.com>
2023-11-13 21:00:21 +0100

    added effects

---
+++ effects/effect2.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO
+import time
+from itertools import chain
+
+arr_pin1 = [3,5,7,8]
+arr_pin2 = [26,11,12,13]
+arr_pin3 = [15,16,18,19]
+arr_pin4 = [21,22,23,24]
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+
+for x in chain(arr_pin1,arr_pin2,arr_pin3,arr_pin4):
+  GPIO.setup(x, GPIO.OUT)
+
+for i in range(10):
+    for x in arr_pin1:
+      GPIO.output(x, True)
+    time.sleep(0.5)
+    for x in arr_pin1:
+      GPIO.output(x, False)
+    time.sleep(0.5)
+    for x in arr_pin2:
+      GPIO.output(x, True)
+    time.sleep(0.5)
+    for x in arr_pin2:
+      GPIO.output(x, False)
+    time.sleep(0.5)
+    for x in arr_pin3:
+      GPIO.output(x, True)
+    time.sleep(0.5)
+    for x in arr_pin3:
+      GPIO.output(x, False)
+    time.sleep(0.5)
+    for x in arr_pin4:
+      GPIO.output(x, True)
+    time.sleep(0.5)
+    for x in arr_pin4:
+      GPIO.output(x, False)
+    time.sleep(0.5)
+
+
